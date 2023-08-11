1bc04bc74476caa340d8de19d038e535ffe8e7b6
pangolin <pangolin@example.com>
2023-08-11 15:03:47 +0800

    添加转义字段 SQL

--- resources/sql/v2.sql
+++ resources/sql/v2.sql
@@ -1,8 +1,2 @@
-alter table gridfs_file
-    modify id bigint auto_increment;
-
-alter table gridfs_chunk
-    modify id bigint auto_increment;
-
-ALTER TABLE elastic
-    ADD hosts JSON NULL AFTER password;
+ALTER TABLE `store`
+    add `escape` TINYINT(1) NULL AFTER `desc`;
